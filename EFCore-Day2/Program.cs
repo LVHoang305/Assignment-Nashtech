1104b1e93f61b01dd0034112b111c2cc1fe4e95e
L.V. Hoang <111042444+zKuma@users.noreply.github.com>
2024-05-13 10:54:41 +0700

    Update Program.cs

--- EFCore-Day2/Program.cs
+++ EFCore-Day2/Program.cs
@@ -17,6 +17,7 @@ public class Program
         builder.Services.AddScoped<IProjectEmployeeService, ProjectEmployeesService>();
         builder.Services.AddScoped<IProjectsService, ProjectsService>();
         builder.Services.AddScoped<ISalariesService, SalariesService>();
+        
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
